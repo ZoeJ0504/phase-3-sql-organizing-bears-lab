384869ca2166f5f7dad8107f401d1db471cf717c
ZoeMendez <zoemendez@Zoes-Air.lan>
2022-09-07 12:49:36 -0700

    I understand

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'M', 'white', 'vicious', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'F', 'black', 'calm', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'brown', 'sassy', true );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 22, 'M', 'black', 'harsh', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 48, 'F', 'white', 'kind', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 14, 'M', 'black and white', 'mean', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 5, 'F', 'blue', 'depressed', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 12, 'M', 'purple', 'caring', false);
